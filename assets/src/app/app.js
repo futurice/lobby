d116c1f9e99e447862f28b11649f9a53de0b3972
Lauri Luotola <lauri.luotola@gmail.com>
2014-11-29 19:55:36 +0200

    Disable angular html5 mode

--- assets/src/app/app.js
+++ assets/src/app/app.js
@@ -17,18 +17,7 @@ angular.module( 'lobby', [
 .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
   function myAppConfig ( $stateProvider, $urlRouterProvider, $locationProvider ) {
 
-	$urlRouterProvider.otherwise(function ($injector, $location) {
-
-		if ($location.$$url === '/') {
-			window.location = '/home';
-		}
-		else {
-			// pass through to let the web server handle this request
-
-			window.location = $location.$$absUrl;
-		}
-	});
-	$locationProvider.html5Mode(true);
+	$urlRouterProvider.otherwise('/');
 }])
 
 .run( function run () {

ab4bd8f189e727d6981342452e13d03c486885e2
Lauri Luotola <lauri.luotola@gmail.com>
2014-12-05 19:08:05 +0200

    Add API endpoint to serve employee listing

--- api/controllers/EmployeeController.js
+++ api/controllers/EmployeeController.js
@@ -1,10 +1,28 @@
 /**
  * EmployeeController
  *
- * @description ::
- * @docs        :: http://sailsjs.org/#!documentation/controllers
+ * @description :: Server-side logic for managing employees
+ * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+
+var fs = require('fs');
 
 module.exports = {
 
+  /**
+   * `EmployeeController.index()`
+   */
+  index: function (req, res) {
+    var employees = fs.readFileSync('./employees.json', { 'encoding': 'utf8'});
+    return res.json(employees);
+  },
+
+  /**
+   * `EmployeeController.notify()`
+   */
+  notify: function (req, res) {
+    return res.json({
+      todo: 'notify() is not implemented yet!'
+    });
+  }
 };

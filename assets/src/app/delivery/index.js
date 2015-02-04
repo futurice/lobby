2ab7ddf69b3ada0c75ea604d9cd0e123e1ac928d
Teivo Markus <markus.teivo@aalto.fi>
2015-02-04 16:30:50 +0200

    delivery messages

--- assets/src/app/delivery/index.js
+++ assets/src/app/delivery/index.js
@@ -12,5 +12,39 @@ angular.module( 'lobby.delivery', [
 	});
 }])
 
-.controller( 'DeliveryCtrl',['$scope', function DeliveryController( $scope ) {
+.controller( 'DeliveryCtrl',['$scope', 'config', '$state', 
+  function DeliveryController( $scope, config, $state ) {
+
+  	$scope.dType = "other";
+  	$scope.debug = "depuk";
+
+	$scope.notify = function(dType) {
+	    
+	    switch (dType) {
+			case "food":
+				var msg = "Futurice Lobby - You have food";
+				break;
+			case "package":
+				var msg = "Futurice Lobby - You have a package";
+				break;
+			case "other":
+				var msg = "Futurice Lobby - You have a delivery";
+				break;
+			default:
+				var msg = "Futurice Lobby - Delivery";
+	    }
+	    $scope.debug = msg;
+	    /*
+	    if ($scope.notificationMessage != "") {
+	      msg += ': "'' + $scope.notificationMessage + '"';
+	    }
+	    $http.put("/api/notify",
+	      {
+	        "type": "flowdock",
+	        "message": msg
+	      }
+	    );
+	    */
+	    //$state.go("finish.delivery");
+	}
 }]);

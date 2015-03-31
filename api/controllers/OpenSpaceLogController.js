51e585b17bb2177770dc27bc45aedd89b3abce7b
Lauri Luotola <lauri.luotola@gmail.com>
2015-03-31 23:34:08 +0300

    More refactoring to api, add pubsub to openspace controller

--- api/controllers/OpenSpaceLogController.js
+++ api/controllers/OpenSpaceLogController.js
@@ -1,16 +1,15 @@
 module.exports = {
   getAll: function(req, res) {
-  	OpenSpaceLog.find().populate('user').exec(function(err, found) {
+  	OpenSpaceLog.find().populate('user').exec(function(err, log) {
       //return if error occurs while fetching log
-      if (err){
-          SystemEvent.add("ERROR", "User log: "+err);
-          return res.json(503,{err:"Error while retrieving userlog"});
+      if (err) {
+        SystemEvent.add("ERROR", "User log: "+err);
+        res.json(503,{err:"Error while retrieving open space log."});
       }
-      //return empty JSON if there is no log entries
-      if (!found.length) {
-          return res.json({});
-      }
-      return found;
-  });
+      console.log("subscribing to oslog changes");
+      OpenSpaceLog.watch(req);
+      OpenSpaceLog.subscribe(req.socket, log);
+      res.json(log);
+    });
   },
 };
